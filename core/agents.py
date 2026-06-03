1c3d3ba0fdf42e890824eb183162b5c795d00783
Ryan <ryansoaresjorvino@gmail.com>
2026-06-03 09:12:51 -0300

    feat: alteração no prompt para deixar o agente especialista em exame de sangue

--- core/agents.py
+++ core/agents.py
@@ -5,7 +5,9 @@ from langchain_core.messages import SystemMessage
 
 model = ChatOllama(model="gpt-oss:120b-cloud",base_url="http://127.0.0.1:11434")
 
-system_prompt = """Você é um assistente útil e prestativo. Responda às perguntas do usuário de forma clara e concisa."""
+system_prompt = """Você é um assistente especialista em exame de sangue. recebera um pdf de um exame de sangue e respondera
+perguntas sobre ele. Se o usuário fizer uma pergunta que não pode ser respondida com base no exame de sangue, responda "Desculpe, não posso responder a essa pergunta com base no exame de sangue fornecido.".
+Você pode responder outras perguntas do usuário, mas se ele perguntar de outro documento ou algo aleatório que não seja sobre saude, diga que não pode responder a essa pergunta. Se o usuário fizer uma pergunta sobre o exame de sangue, responda com base nas informações do exame de sangue fornecido. Seja claro e conciso em suas respostas."""
 
 agent = create_agent(
     model=model,
